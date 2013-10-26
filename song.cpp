e6ad85e59383d5916ec163b37345a47129afddc5
Tobi Hahn <cmpgk1024@gmail.com>
2013-10-26 13:49:07 -0400

    finished allowed note definitions for all major keys

--- song.cpp
+++ song.cpp
@@ -58,5 +58,62 @@ Song::Song(vector<vector<int> > d, Note key){
 			allowedNotes.push_back(Gs);
 			allowedNotes.push_back(As);
 			break;
+		case Fs:
+			allowedNotes.push_back(Fs);
+			allowedNotes.push_back(Gs);
+			allowedNotes.push_back(As);
+			allowedNotes.push_back(B);
+			allowedNotes.push_back(Cs);
+			allowedNotes.push_back(Ds);
+			allowedNotes.push_back(Es);
+			break;
+		case Df:
+			allowedNotes.push_back(Df);
+			allowedNotes.push_back(Ef);
+			allowedNotes.push_back(F);
+			allowedNotes.push_back(Gf);
+			allowedNotes.push_back(Af);
+			allowedNotes.push_back(Bf);
+			allowedNotes.push_back(C);
+			break;
+		case Af:
+			allowedNotes.push_back(Af);
+			allowedNotes.push_back(Bf);
+			allowedNotes.push_back(C);
+			allowedNotes.push_back(Df);
+			allowedNotes.push_back(Ef);
+			allowedNotes.push_back(F);
+			allowedNotes.push_back(G);
+			break;
+		case Ef:
+			allowedNotes.push_back(Ef);
+			allowedNotes.push_back(F);
+			allowedNotes.push_back(G);
+			allowedNotes.push_back(Af);
+			allowedNotes.push_back(Bf);
+			allowedNotes.push_back(C);
+			allowedNotes.push_back(D);
+			break;
+		case Bf:
+			allowedNotes.push_back(Bf);
+			allowedNotes.push_back(C);
+			allowedNotes.push_back(D);
+			allowedNotes.push_back(Ef);
+			allowedNotes.push_back(F);
+			allowedNotes.push_back(G);
+			allowedNotes.push_back(A);
+			break;
+		case F:
+			allowedNotes.push_back(F);
+			allowedNotes.push_back(G);
+			allowedNotes.push_back(A);
+			allowedNotes.push_back(Bf);
+			allowedNotes.push_back(C);
+			allowedNotes.push_back(D);
+			allowedNotes.push_back(E);
+			break;
+		default:
+			//throw exception
+			break;
 	}
 }
--- song.h
+++ song.h
@@ -1,5 +1,5 @@
 #include <vector>
-#include "key.h"
+#include "note.h"
 using namespace std;
 
 class Song{
